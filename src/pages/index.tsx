da4d92d4392a5c1daac74a39ad6b919b4de69130
Raphael Arce <raphael.arce@ts.berlin>
2025-01-08 14:24:52 +0100

    feat: added layout file and an eslint rule
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Card from "../components/card.tsx";
 import WeatherCard from "../components/weather-card";
-import { Link } from "react-router";
 import { useI18nStore } from "../i18n/i18n-store";
-import { LanguageToggle } from "../components/router/languageToggle";
+import Layout from "../layout/layout";
 import { data } from "../data";
 
 const chartIds = Object.keys(data) as Array<keyof typeof data>;
@@ -11,16 +10,11 @@ export default function Index() {
 	const i18n = useI18nStore((state) => state.i18n());
 
 	return (
-		<main>
-			<LanguageToggle />
+		<Layout>
 			<h1 className="text-2xl font-semibold p-5">{i18n["header.title"]}</h1>
 			<br />
 
-			<Link to="/current-projects/" className="text-blue-500 underline p-5">
-				link aktuelle Projekte
-			</Link>
-
-			<WeatherCard></WeatherCard>
+			<WeatherCard />
 
 			<br />
 
@@ -29,6 +23,6 @@ export default function Index() {
 					<Card key={id.toString()} id={id} />
 				))}
 			</div>
-		</main>
+		</Layout>
 	);
 }
