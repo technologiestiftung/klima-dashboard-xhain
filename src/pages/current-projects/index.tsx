c26769ae3fb5f19ee64797427cc41515882d0921
Raphael Arce <raphael.arce@ts.berlin>
2025-01-08 14:24:52 +0100

    feat: added layout file and an eslint rule
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/pages/current-projects/index.tsx
+++ src/pages/current-projects/index.tsx
@@ -1,15 +1,9 @@
-import { Link } from "react-router";
+import Layout from "../../layout/layout";
 
 export default function Index() {
 	return (
-		<main>
+		<Layout>
 			<h1 className="text-2xl font-semibold p-5">Aktuelle Projekte</h1>
-
-			<br />
-
-			<Link to="/" className="text-blue-500 underline p-5">
-				link home
-			</Link>
-		</main>
+		</Layout>
 	);
 }
