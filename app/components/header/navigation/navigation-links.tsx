876df4e69488837e22419694079ef198b620a256
Zainab Tariq <zainabtariq12@gmail.com>
2025-01-22 09:46:10 +0100

    feat: update styles

--- app/components/header/navigation/navigation-links.tsx
+++ app/components/header/navigation/navigation-links.tsx
@@ -14,7 +14,11 @@ const navLinks: {
 	labelKey: AvailableTranslations;
 	className?: string;
 }[] = [
-	{ path: "/", labelKey: "header.nav.dashboard", className: "lg:hidden" },
+	{
+		path: "/",
+		labelKey: "header.nav.dashboard",
+		className: "lg:hidden",
+	},
 	{ path: "/climate-concepts/", labelKey: "header.nav.climateConcepts" },
 	{ path: "/current-projects/", labelKey: "header.nav.currentProjects" },
 	{ path: "/about/", labelKey: "header.nav.about" },
@@ -26,16 +30,14 @@ export const NavigationLinks: React.FC<NavigationLinksProps> = ({
 	const currentLanguage = useLanguage();
 
 	return (
-		<ul className="flex flex-col lg:flex-row gap-7 lg:gap-0 justify-center items-center lg:items-start h-full text-xhain-blue-100 font-bold text-2xl leading-8 lg:text-xl lg:leading-7">
+		<ul className="flex flex-col lg:flex-row gap-7 lg:gap-0 justify-center items-center h-full text-xhain-blue-100 font-bold text-2xl leading-8 lg:text-xl lg:leading-7">
 			{/* navigation links */}
 			{navLinks.map(({ path, labelKey, className }) => (
-				<li
-					key={path}
-					className={`lg:py-7 lg:px-6 hover:underline hover:underline-offset-8 hover:decoration-1 lg:hover:no-underline lg:hover:bg-xhain-blue-20 lg:focus:bg-xhain-blue-20 focus-xhain-blue-80 ${className ?? ""}`}
-				>
+				<li key={path}>
 					<Link
 						to={buildLocalizedLink(path, currentLanguage)}
 						onClick={closeMenu}
+						className={`inline-block lg:p-6 hover:underline hover:underline-offset-8 hover:decoration-1 lg:hover:no-underline lg:hover:bg-xhain-blue-20 lg:focus:bg-xhain-blue-20 focus-xhain-blue-80 ${className}`}
 					>
 						{i18n(labelKey)}
 					</Link>
@@ -43,10 +45,11 @@ export const NavigationLinks: React.FC<NavigationLinksProps> = ({
 			))}
 
 			{/* cta button */}
-			<li className="w-fit flex items-center p-[10px] lg:py-7 lg:px-6 mt-9 lg:mt-0 bg-xhain-blue-50 text-white hover:bg-xhain-blue-60 focus-xhain-blue-80">
+			<li>
 				<Link
 					to={buildLocalizedLink("/act-now", currentLanguage)}
 					onClick={closeMenu}
+					className="inline-block items-center p-2.5 lg:p-6 mt-9 lg:mt-0 bg-xhain-blue-50 text-white hover:bg-xhain-blue-60 focus-xhain-blue-80"
 				>
 					{i18n("header.cta")}
 				</Link>
