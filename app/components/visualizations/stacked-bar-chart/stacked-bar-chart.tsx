944c8e5420a1b3b6f0907525346181ee3ec97829
aeschi <anna.eschenbacher@icloud.com>
2025-01-23 13:50:45 +0100

    feat: cleanup

--- app/components/visualizations/stacked-bar-chart/stacked-bar-chart.tsx
+++ app/components/visualizations/stacked-bar-chart/stacked-bar-chart.tsx
@@ -5,10 +5,10 @@ import { formatPercentage, i18n } from "~/i18n/i18n-utils";
 const { heatingMix2021Summarized } = howXhainContributesData;
 
 const colors = [
-	"bg-xhain-green-40",
-	"bg-xhain-green-50",
-	"bg-xhain-green-60",
 	"bg-xhain-green-80",
+	"bg-xhain-green-60",
+	"bg-xhain-green-50",
+	"bg-xhain-green-40",
 ];
 
 export const StackedBarChart: React.FC = () => {
@@ -18,24 +18,22 @@ export const StackedBarChart: React.FC = () => {
 				{i18n(`chart.heatingMix2021Summarized.description`)}
 			</p>
 			<div className="flex flex-col h-[640px]">
-				{heatingMix2021Summarized
-					.map((data, index) => (
-						<div
-							key={index}
-							className={`w-full flex justify-between items-end h-fit relative rounded-lg min-h-[2px] ${colors[index]}`}
-							style={{
-								height: data.percentage * 100 + "%",
-							}}
-						>
-							<span className={`font-bold m-2.5`}>
-								{i18n(`chart.heatingMix2021Summarized.${data.source}`)}
-							</span>
-							<span className={`font-bold m-2.5`}>
-								{formatPercentage(data.percentage)}
-							</span>
-						</div>
-					))
-					.reverse()}
+				{heatingMix2021Summarized.map((data, index) => (
+					<div
+						key={index}
+						className={`w-full flex justify-between items-end h-fit relative rounded-lg min-h-[2px] ${colors[index]}`}
+						style={{
+							height: data.percentage * 100 + "%",
+						}}
+					>
+						<span className={`font-bold m-2.5`}>
+							{i18n(`chart.heatingMix2021Summarized.${data.source}`)}
+						</span>
+						<span className={`font-bold m-2.5`}>
+							{formatPercentage(data.percentage)}
+						</span>
+					</div>
+				))}
 			</div>
 		</div>
 	);
