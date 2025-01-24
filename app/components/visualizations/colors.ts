34ea301d33a7d5bff775b6b1da52adec1270fb27
Zainab Tariq <zainabtariq12@gmail.com>
2025-01-24 14:53:21 +0100

    feat: add colors for svg

--- app/components/visualizations/colors.ts
+++ app/components/visualizations/colors.ts
@@ -8,3 +8,4 @@ export const xhainBlue40 = fullConfig.theme.colors["xhain-blue-40"];
 export const xhainBlue50 = fullConfig.theme.colors["xhain-blue-50"];
 export const xhainBlue60 = fullConfig.theme.colors["xhain-blue-60"];
 export const xhainBlue80 = fullConfig.theme.colors["xhain-blue-80"];
+export const xhainGreen50 = fullConfig.theme.colors["xhain-green-50"];
