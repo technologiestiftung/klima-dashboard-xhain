09f640b1e1af80d4717e5c42609bb6f4bdff0052
aeschi <anna.eschenbacher@icloud.com>
2025-01-29 14:49:00 +0100

    feat: refafctor

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -10,9 +10,7 @@ import { LineChart } from "~/components/visualizations/line-chart/line-chart";
 import { StackedBarChart } from "~/components/visualizations/stacked-bar-chart/stacked-bar-chart";
 import { BarChartThg } from "~/components/visualizations/bar-chart-thg/bar-chart-thg";
 import { BarChartModalSplit } from "~/components/visualizations/bar-chart-modal-split/bar-chart-modal-split";
-import { LinkArrowIcon } from "./icon/link-arrow-icon";
-import { ExternalAnchorLink } from "./anchor-link/external-anchor-link";
-import Markdown from "react-markdown";
+import { DialogContent } from "./dialog/dialog-content";
 
 interface CardProps {
 	id: keyof typeof howXhainContributesData | keyof typeof consequencesData;
@@ -84,13 +82,6 @@ const Card: React.FC<CardProps> = ({ id }) => {
 	const title = i18n(`chart.${id}.title`);
 	const subTitle = i18n(`chart.${id}.subtitle`);
 
-	const description = i18n(`chart.infoDialog.${id}.description`);
-	const sourceTitle = i18n(`chart.infoDialog.${id}.sourceTitle`);
-	const sourceLabel = i18n(`chart.infoDialog.${id}.sourceLabel`);
-
-	const learnMoreLabel = i18n(`chart.infoDialog.learnMoreLabel`);
-	const learnMoreLink = i18n(`chart.infoDialog.${id}.learnMoreLink`);
-
 	const { size, color, component: Chart } = charts[id];
 
 	const data =
@@ -152,38 +143,7 @@ const Card: React.FC<CardProps> = ({ id }) => {
 			</div>
 
 			<Dialog id={dialogId}>
-				<div className="flex flex-col gap-y-5 ">
-					<div className="flex flex-col">
-						<h2 className="text-xl leading-7 font-bold">{title}</h2>
-						<p className="text-xl leading-7 font-normal ">{subTitle}</p>
-					</div>
-
-					<p className="font-normal text-base leading-6 overflow-y-scroll max-h-[312px] dialog-scrollbar pr-2.5">
-						<Markdown className="markdown-container">{description}</Markdown>
-					</p>
-
-					<div className="flex flex-row text-sm leading-5 gap-2.5">
-						<p className="font-semibold">{sourceTitle}</p>
-						<Markdown
-							// @ts-expect-error typing too complex
-							components={{ a: ExternalAnchorLink }}
-							className="markdown-container"
-						>
-							{sourceLabel}
-						</Markdown>
-					</div>
-
-					<div className="flex flex-row justify-between gap-x-5">
-						<a
-							href={learnMoreLink}
-							className="flex w-fit items-center leading-6 text-base px-2.5 py-1 gap-x-2 border-2 border-xhain-blue-50 text-xhain-blue-50 rounded-full font-semibold 
-							hover:border-xhain-blue-80 hover:text-xhain-blue-80 focus:outline focus:outline-3 focus:outline-xhain-blue-80 focus:outline-offset-5"
-						>
-							{learnMoreLabel}
-							<LinkArrowIcon />
-						</a>
-					</div>
-				</div>
+				<DialogContent id={id} />
 			</Dialog>
 		</figure>
 	);
