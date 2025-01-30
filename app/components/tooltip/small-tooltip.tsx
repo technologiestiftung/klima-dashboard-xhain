d3493cdeb08cd6b8f3c119cca3dfbb8e7974bba6
Raphael Arce <raphael.arce@ts.berlin>
2025-01-30 21:03:35 +0100

    feat: add touch support on small tooltip
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- app/components/tooltip/small-tooltip.tsx
+++ app/components/tooltip/small-tooltip.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { i18n } from "~/i18n/i18n-utils.ts";
 
 export function SmallTooltip({ children }: { children?: React.ReactNode }) {
+	const [isTapped, setIsTapped] = useState(false);
+
 	return (
 		<>
 			<div className="group flex gap-x-2">
-				<button className="peer">
+				<button className="peer" onTouchStart={() => setIsTapped(!isTapped)}>
 					<img
 						className="size-5"
 						src="/images/i-icon.svg"
@@ -15,7 +17,7 @@ export function SmallTooltip({ children }: { children?: React.ReactNode }) {
 				<div
 					className={`
 					text-sm leading-5 text-xhain-blue-50 bg-white max-w-24 md:max-w-none px-0.5 md:px-1.5 rounded-md underline
-					opacity-0
+					${isTapped ? "opacity-100" : "opacity-0"}
 					group-focus:opacity-100 group-hover:opacity-100
 					peer-focus:opacity-100 focus-hover:opacity-100
 				 	focus-within:opacity-100 hover-within:opacity-100
