ab1a4469b10c3306613f7c84e4e7e2983c83642a
Zainab Tariq <zainabtariq12@gmail.com>
2025-02-03 09:46:23 +0100

    feat: review changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -83,7 +83,7 @@ export default function Index() {
 				</div>
 			</section>
 
-			<section className="px-5 lg:px-20">
+			<section className="px-5 lg:px-20 max-w-lg m-auto">
 				<FeatureSection />
 			</section>
 		</>
