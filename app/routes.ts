772c3e2ab641e5ea9cd23fb89a72315f1fc1babb
Raphael Arce <raphael.arce@ts.berlin>
2025-02-03 10:38:27 +0100

    feat: add routing and pages
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,9 @@ import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
 	route(":lang?/", "./routes/index.tsx"),
+	route(":lang?/about/", "./routes/about/index.tsx"),
+	route(":lang?/climate-concepts/", "./routes/climate-concepts/index.tsx"),
 	route(":lang?/current-projects/", "./routes/current-projects/index.tsx"),
+	route(":lang?/imprint/", "./routes/imprint/index.tsx"),
+	route(":lang?/privacy/", "./routes/privacy/index.tsx"),
 ] satisfies RouteConfig;
