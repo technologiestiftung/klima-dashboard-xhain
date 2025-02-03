d9aa028a5e153d3fc103821d042e3dddd2522f8f
Raphael Arce <raphael.arce@ts.berlin>
2025-02-03 10:38:27 +0100

    feat: add routing and pages
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,19 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// return a list of URLs to prerender at build time
 	async prerender() {
-		return ["/", "/en/", "/current-projects/", "/en/current-projects/"];
+		return [
+			"/",
+			"/en/",
+			"/about/",
+			"/en/about/",
+			"/current-projects/",
+			"/en/current-projects/",
+			"/climate-concepts/",
+			"/en/climate-concepts/",
+			"/imprint/",
+			"/en/imprint/",
+			"/privacy/",
+			"/en/privacy/",
+		];
 	},
 } satisfies Config;
