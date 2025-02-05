a211ab5a392eef73a7931d8edaad774274898572
Zainab Tariq <zainabtariq12@gmail.com>
2025-02-05 08:23:45 +0100

    fix: feature section cta label text (#57)

--- app/i18n/translations/en.ts
+++ app/i18n/translations/en.ts
@@ -399,7 +399,7 @@ Regarding the number of heavy rainfall days, no clear trend has been observed ye
 	"featureSection.title": "What is already happening in XHain?",
 	"featureSection.description":
 		"In Xhain, numerous projects are already being implemented to address these challenges. Here, we give you an insight into some of the existing projects.",
-	"featureSection.cta": "Get to know all the projects",
+	"featureSection.cta": "Current projects",
 
 	"footer.cardHeading":
 		"Do you have any questions about the climate dashboard and the climate concepts?",
