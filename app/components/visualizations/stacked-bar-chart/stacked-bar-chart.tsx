960b07604e0c4eee43e9be042be5b7a55e9fba6a
aeschi <56318362+aeschi@users.noreply.github.com>
2025-02-05 09:15:11 +0100

    fix: padding for hidden label (#61)

--- app/components/visualizations/stacked-bar-chart/stacked-bar-chart.tsx
+++ app/components/visualizations/stacked-bar-chart/stacked-bar-chart.tsx
@@ -14,7 +14,7 @@ const colors = [
 export const StackedBarChart: React.FC = () => {
 	return (
 		<div className="w-full flex flex-col h-fit">
-			<div className="flex flex-col h-[400px] desktop:h-[670px]">
+			<div className="flex flex-col h-[400px] desktop:h-[670px] pt-6">
 				{heatingMix2021Summarized.map((data, index) => (
 					<div
 						key={index}
