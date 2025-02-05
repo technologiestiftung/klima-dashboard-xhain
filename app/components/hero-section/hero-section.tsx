12d46434434166ed6abf10ace034f2c878ca8b57
aeschi <anna.eschenbacher@icloud.com>
2025-02-05 12:25:33 +0100

    fix: typo

--- app/components/hero-section/hero-section.tsx
+++ app/components/hero-section/hero-section.tsx
@@ -7,13 +7,13 @@ export const HeroSection: React.FC = () => {
 		<div className="grid grid-cols-1 lg:grid-cols-2 items-center h-vhs lg:h-[calc(100svh-125px)] max-w-8xl w-full hero-section">
 			<div className="h-full bg-xhain-green-50">
 				<div className="flex flex-col p-5 md:px-20 lg:py-0 w-full h-full justify-center items-center lg:items-start text-center lg:text-left">
-					<div className="mb-0 md:mb-2 text-base leading-6 font-normal md:text-xl md:leaading-7 md:font-bold text-left">
+					<div className="mb-0 md:mb-2 text-base leading-6 font-normal md:text-xl md:leading-7 md:font-bold text-left">
 						{i18n("heroSection.eyebrowText")}
 					</div>
-					<h1 className="w-full mb-4 md:mb-7 text-2xl leading-8 font-bold md:text-5xl xl:text-6xl 2xl:text-7xl md:leaading-none :font-semibold">
+					<h1 className="w-full mb-4 md:mb-7 text-2xl leading-8 font-bold md:text-5xl xl:text-6xl 2xl:text-7xl md:leading-none :font-semibold">
 						{i18n("heroSection.heading")}
 					</h1>
-					<p className="text-base leading-6 font-normal md:text-2xl xl:text-3xl 2xl:text-3xl md:leaading-9">
+					<p className="text-base leading-6 font-normal md:text-2xl xl:text-3xl 2xl:text-3xl md:leading-9">
 						{i18n("heroSection.subheading")}
 					</p>
 				</div>
