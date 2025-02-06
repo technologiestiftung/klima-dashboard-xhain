5164d145b75af3fdc8fbbcfc391d0374ce61edce
Raphael Arce <raphael.arce@ts.berlin>
2025-02-06 10:50:45 +0100

    test: add a11y tests
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- tests/a11y/example.spec.ts
+++ tests/a11y/example.spec.ts
@@ -1,15 +1,33 @@
 import { test, expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
+import fs from "fs";
+import config from "react-router.config.ts";
+import type { Result } from "axe-core";
+
+const routes = await config.prerender();
 
 test.describe("homepage", () => {
 	test("should not have any automatically detectable accessibility issues", async ({
 		page,
 	}) => {
-		await page.goto("/");
+		const routeViolations: Record<string, Result[]> = {};
 
-		// @ts-expect-error AxeBuilder is not typed
-		const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+		for (const route of routes) {
+			await page.goto(route);
 
-		expect(accessibilityScanResults.violations).toEqual([]);
+			// @ts-expect-error AxeBuilder is not typed
+			const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+			routeViolations[route] = accessibilityScanResults.violations;
+		}
+
+		fs.writeFileSync(
+			`./test-results/a11y-violations.json`,
+			JSON.stringify(routeViolations, null, 2),
+		);
+
+		for (const [_, violations] of Object.entries(routeViolations)) {
+			expect(violations).toEqual([]);
+		}
 	});
 });
