2a7dd40d7a55bad6a6c5d383d2d3d6b1103f39e8
Zainab Tariq <zainabtariq12@gmail.com>
2025-02-06 15:43:55 +0100

    feat: center content

--- app/routes/imprint/index.tsx
+++ app/routes/imprint/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
 			<h1 className="py-10 px-2 lg:px-20 text-4xl leading-10 font-semibold lg:text-7xl lg:leading-none text-center lg:text-left bg-xhain-blue-20">
 				{i18n("imprint.title")}
 			</h1>
-			<div className="max-w-8xl px-5 py-10 lg:p-20 lg:pb-0">
+			<div className="max-w-8xl 2xl:mx-auto px-5 py-10 lg:p-20 lg:pb-0">
 				<p className="mb-3 text-2xl leading-8 font-bold">
 					{i18n("imprint.heading")}
 				</p>
