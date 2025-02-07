90a2d64f980fda439cf72240c7e42cf529dba57f
Zainab Tariq <zainabtariq12@gmail.com>
2025-02-07 14:01:28 +0100

    fix: imprint and privacy policy urls

--- app/components/footer/footer-left-section.tsx
+++ app/components/footer/footer-left-section.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { i18n } from "~/i18n/i18n-utils";
+import { i18n, buildLocalizedLink } from "~/i18n/i18n-utils";
 import { Link } from "react-router";
 const xhainLogo = "/images/xhain-logo-negative.svg";
 
@@ -13,13 +13,13 @@ export const FooterLeftSection: React.FC = () => {
 			/>
 			<div className="flex flex-col gap-5 md:gap-4 items-center lg:items-start text-white text-xl leading-7 md:text-2xl md:leading-8 lg:text-xl lg:leading-7 font-semibold">
 				<Link
-					to="/imprint/"
+					to={buildLocalizedLink("/imprint/")}
 					className="w-fit hover:underline hover:underline-offset-8 hover:decoration-1 focus-white"
 				>
 					{i18n("footer.imprint")}
 				</Link>
 				<Link
-					to="/privacy/"
+					to={buildLocalizedLink("/privacy/")}
 					className="w-fit hover:underline hover:underline-offset-8 hover:decoration-1 focus-white"
 				>
 					{i18n("footer.privacyPolicy")}
