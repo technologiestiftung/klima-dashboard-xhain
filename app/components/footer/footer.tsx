7ff56480aa7a051b7cc2b8b3619a5d9455a16dcf
aeschi <anna.eschenbacher@icloud.com>
2025-02-10 11:32:26 +0100

    feat: adapt contact card width mobile

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -6,8 +6,8 @@ import { FooterLeftSection } from "./footer-left-section";
 export const Footer: React.FC = () => {
 	return (
 		<footer className="bg-xhain-blue-50 mt-56 lg:mt-72">
-			<div className="max-w-8xl px-20 py-10 m-auto">
-				<div className="relative container h-max">
+			<div className="max-w-8xl px-5 sm:px-20 py-10 mx-auto w-full">
+				<div className="relative container h-max w-full mx-auto">
 					<FooterCardSection />
 					<FooterLeftSection />
 				</div>
