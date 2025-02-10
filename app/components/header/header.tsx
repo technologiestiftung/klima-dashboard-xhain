14f5944314227e4227d1a0e932b2bf0a3f8dda2d
Zainab Tariq <zainabtariq12@gmail.com>
2025-02-10 13:30:20 +0100

    feat: rename variable

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -9,14 +9,14 @@ const xhainLogo = "/images/xhain-logo.svg";
 export const Header: React.FC = () => {
 	const scrollDirection = useScrollDirection();
 	const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
-	const shouldShowHeader = !isMobile || scrollDirection === "up";
+	const isHeaderVisible = !isMobile || scrollDirection === "up";
 
 	return (
 		<header
 			className={`
         sticky top-0 flex justify-between items-center pl-5 lg:pl-20 z-50 bg-white
         transition-transform duration-500
-        ${shouldShowHeader ? "translate-y-0" : "-translate-y-full"}
+        ${isHeaderVisible ? "translate-y-0" : "-translate-y-full"}
       `}
 		>
 			<Link to={buildLocalizedLink("/")}>
