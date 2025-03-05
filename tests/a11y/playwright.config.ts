2588a65b30ec8ae574a5fbbf8e625b5dbb1b48cb
aeschi <anna.eschenbacher@ts.berlin>
2025-03-05 10:40:40 +0100

    feat: increase timeout

--- tests/a11y/playwright.config.ts
+++ tests/a11y/playwright.config.ts
@@ -16,7 +16,7 @@ const port = "3000"; // port on preview mode (serve)
  */
 export default defineConfig({
 	testDir: "./",
-	timeout: 10000,
+	timeout: 15000,
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
