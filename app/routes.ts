89243276e7c8bbbaab44dbaf2c9b8ad257b55e0e
aeschi <anna.eschenbacher@ts.berlin>
2025-04-28 15:34:34 +0200

    feat: adjust blue footer and cleanup

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,4 @@ export default [
 	route(":lang?/about/", "./routes/about/index.tsx"),
 	route(":lang?/climate-strategies/", "./routes/climate-strategies/index.tsx"),
 	route(":lang?/current-projects/", "./routes/current-projects/index.tsx"),
-	route(":lang?/imprint-privacy/", "./routes/imprint-privacy/index.tsx"),
 ] satisfies RouteConfig;
