b41dfe06457235195a4e7bfed0c09456415adb00
aeschi <anna.eschenbacher@ts.berlin>
2025-05-07 13:49:29 +0200

    feat: exchange logo

--- app/components/footer/footer-left-section.tsx
+++ app/components/footer/footer-left-section.tsx
@@ -3,9 +3,13 @@ import { i18n } from "~/i18n/i18n-utils";
 
 export const FooterLeftSection: React.FC = () => {
 	return (
-		<div className="flex flex-col gap-[60px] md:gap-20 items-center lg:items-start pt-40 lg:pt-0">
-			<span className="hidden md:flex md:h-[30px]" />
-			<div className="flex flex-col gap-5 md:gap-4 items-center lg:items-start text-white text-xl leading-7 md:text-2xl md:leading-8 lg:text-xl lg:leading-7 font-semibold">
+		<div className="flex flex-col items-center lg:items-start pt-28 md:pt-40 lg:pt-0">
+			<img
+				src="/images/klimadashboard_logo_inverted.svg"
+				alt="Xhain Logo"
+				className="h-14"
+			/>
+			<div className="flex flex-col gap-5 md:gap-4 mt-12 items-center lg:items-start text-white text-xl leading-7 md:text-2xl md:leading-8 lg:text-xl lg:leading-7 font-semibold">
 				<a
 					href="https://www.technologiestiftung-berlin.de/impressum"
 					className="w-fit hover:underline hover:underline-offset-8 hover:decoration-1 focus-white"
--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { buildLocalizedLink, i18n } from "~/i18n/i18n-utils";
+import { buildLocalizedLink } from "~/i18n/i18n-utils";
 import { Navigation } from "./navigation/navigation";
 import { useScrollDirection } from "~/hooks/use-scroll-direction";
 
@@ -17,10 +17,19 @@ export const Header: React.FC = () => {
       `}
 		>
 			<a
-				className="font-bold text-2xl leading-8 xl:text-3xl lg:leading-7 py-4 lg:py-6 flex self-end"
+				className="font-bold text-2xl leading-8 xl:text-3xl lg:leading-7 py-4 lg:py-6 flex self-end lg:self-center"
 				href={buildLocalizedLink("/")}
 			>
-				{i18n("header.climateDashboard.title")}
+				<img
+					src="/images/klimadashboard_logo.svg"
+					alt="Xhain Logo"
+					className="flex sm:hidden h-11"
+				/>
+				<img
+					src="/images/klimadashboard_logo_with_title.svg"
+					alt="Xhain Logo"
+					className="hidden sm:flex h-11"
+				/>
 			</a>
 			<Navigation />
 		</header>
